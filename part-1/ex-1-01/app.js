4c39d0929ea4f3266c962fc391451002661fbc7c
Minna <minna.maaria.laitinen@gmail.com>
2022-07-06 18:59:39 +0300

    exercise 2.01

--- part-1/ex-1-01/app.js
+++
@@ -1,9 +0,0 @@
-const randomHash = Math.random().toString(36).substring(2, 24)
-
-const printTimeAndHash = () => {
-  const currentTime = new Date()
-  console.log(currentTime.toISOString() + ": " + randomHash)
-  setTimeout(printTimeAndHash, 5000)
-}
-
-printTimeAndHash(randomHash)
