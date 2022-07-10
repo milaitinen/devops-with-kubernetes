8ca597a567ac171d2669094dab245c2e180f96df
Minna <minna.maaria.laitinen@gmail.com>
2022-07-10 18:40:45 +0300

    exercise 2.08

---
+++ part2/ex-2.08/todoapp-frontend/src/App.js
@@ -0,0 +1,55 @@
+import './App.css'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
+const backendUrl = 'http://localhost:8081'
+
+const App = () => {
+  const [inputText, setInput] = useState('')
+  const [todoList, setTodoList] = useState([])
+
+  const fetchTodos = async () => {
+    const res = await axios(`${backendUrl}/todos`)
+    setTodoList(res.data)
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const options = { headers: {"content-type": "text/plain"} }
+    await axios.post(`${backendUrl}/todos`, inputText, options)
+    fetchTodos()
+    setInput('')
+  }
+
+  const handleChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <img src={`${backendUrl}/files/image.jpg`} alt='todays-img' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="new-todo-input"
+          value={inputText}
+          onChange={handleChange}
+        />
+        <button type="submit" id="create-todo-btn">
+          Create TODO
+        </button>
+      </form>
+
+      <ul id="todo-list">
+        {todoList.map((task, i) => <li key={i}>{task.todo}</li>)}
+      </ul>
+  </div>
+  );
+}
+
+export default App;
